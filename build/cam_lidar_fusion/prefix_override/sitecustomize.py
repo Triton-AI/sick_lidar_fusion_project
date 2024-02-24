9b4027bb9c2ddf8461a839abb3b813a843fc320c
davidq1688 <davidq1688@gmail.com>
2024-02-24 14:53:03 -0800

    cleanup structure

---
+++ build/cam_lidar_fusion/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/david/projects/sick_lidar_fusion_project/install/cam_lidar_fusion'
--- src/cam_lidar_fusion/build/cam_lidar_fusion/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/tritonai/projects/sick_lidar_fusion_project/src/cam_lidar_fusion/install/cam_lidar_fusion'
